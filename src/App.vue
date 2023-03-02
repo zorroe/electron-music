0cfe6a9ba0d0f0715c6e325fe7198d2ff7fe702b
lxxx <1318753541@qq.com>
2023-03-02 08:58:11 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <HelloWorld msg="Electron + Vite + Vue" />
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<style>
+
+</style>
